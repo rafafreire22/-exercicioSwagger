2907338cec028843953504a409dd00941e020037
rafafreire22 <133990084+rafafreire22@users.noreply.github.com>
2023-11-16 13:40:43 -0300

    Add files via upload

---
+++ src/main/java/com/Rafaellafreire/projeto/Lombok/controllers/UsuarioControler.java
@@ -0,0 +1,81 @@
+package com.Rafaellafreire.projeto.Lombok.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Rafaellafreire.projeto.Lombok.etities.Usuario;
+import com.Rafaellafreire.projeto.Lombok.services.UsuarioService;
+
+import jakarta.validation.Valid;
+
+
+@Tag(name = "Usuarios", description = "API REST DE GENERENCIAMENTO DE USUÁRIO9S")
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioControler {
+	private final UsuarioService usuarioService;
+
+	@Autowired
+	public UsuarioControler(UsuarioService usuarioService) {
+		this.usuarioService = usuarioService;
+	}
+
+	@GetMapping("/{id}")
+	@operation(summary = "localiza usuario por id ")
+	public ResponseEntity<Usuario> findUsuariobyId(@PathVariable Long id) {
+		Usuario usuario = usuarioService.findUsuarioById(id);
+		if (usuario != null) {
+			return ResponseEntity.ok(usuario);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}	
+	@GetMapping
+	@operation(summary = "apresenta todos os usuarios ")
+	public ResponseEntity<List<Usuario>> findAllUsuarioscontrol() {
+		List<Usuario> usuarios = usuarioService.findAllUsuario();
+		return ResponseEntity.ok(usuarios);
+	}
+
+
+	@PostMapping("/")
+	@operation(summary = "cadastra um usuário ")
+	public ResponseEntity<Usuario> insertUsuariosControl(@RequestBody @Valid Usuario usuario) {
+		Usuario novoUsuario = usuarioService.insertUsuario(usuario);
+		return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+	}
+
+	@PutMapping("/id")
+	@operation(summary = "Altera um usuário")
+	public ResponseEntity<Usuario> updateUsuarioControl(@PathVariable Long id, @RequestBody @Valid Usuario usuario) {
+		Usuario mudausuario = usuarioService.updateUsuario(id, usuario);
+		if (mudausuario != null) {
+			return ResponseEntity.ok(usuario);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	@DeleteMapping("/id")
+	@operation(summary = "Exclui um usuário")
+	public ResponseEntity<String> deleteUsuarioControl(@PathVariable Long id) {
+		boolean remover = usuarioService.deleteUsuario(id);
+		if (remover) {
+			return ResponseEntity.ok().body("usuario Excluido com sucesso");
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+}
+		
